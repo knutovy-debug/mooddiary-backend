d7c6e62d59e7ad4549618d7867c5dba98ff628ef
knutovy-debug <knutovy@gmail.com>
2026-09-02 20:51:55 +0500

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -4,6 +4,7 @@ from app.core.database import Base
 
 class User(Base):
     __tablename__ = "users"
+    is_payment_pending = Column(Boolean, default=False)
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(255), unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
