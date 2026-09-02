7498382735a38d4b561cf40b091f9edf83727ac4
knutovy-debug <knutovy@gmail.com>
2026-09-02 21:32:45 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,15 +3,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import Base, sync_engine
 from app.api.v1 import auth, entries, subscription
 
-# ВАЖНО: Создаем таблицы через sync_engine
 Base.metadata.create_all(bind=sync_engine)
 
 app = FastAPI(title="MoodDiary API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False, # Меняем на False, чтобы работало с "*"
+    allow_origins=["*"],   # Разрешаем запросы со всех сайтов
+    allow_credentials=False, # ОБЯЗАТЕЛЬНО False для работы с "*"
     allow_methods=["*"],
     allow_headers=["*"],
 )
